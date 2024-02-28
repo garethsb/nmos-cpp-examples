1f695afa1e8d9a7c3fb55e15e36c81406ac6bb01
Gareth Sylvester-Bradley <garethsb@nvidia.com>
2024-02-28 19:56:20 +0000

    Bump Conan package version to nmos-cpp/cci.20240223 Updated to https://github.com/sony/nmos-cpp/commit/27dff31919e06a132651291648293ff7b6b38b38

--- my-nmos-node/conanfile.py
+++ my-nmos-node/conanfile.py
@@ -6,6 +6,4 @@ class MyNmosNodeRecipe(ConanFile):
     generators = "CMakeDeps"
 
     def requirements(self):
-        self.requires("nmos-cpp/cci.20221203")
-        self.requires("json-schema-validator/2.3.0", override=True)
-        self.requires("nlohmann_json/3.11.3", override=True)
+        self.requires("nmos-cpp/cci.20240223")
